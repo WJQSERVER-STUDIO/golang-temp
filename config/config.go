81c04fec519dd455d7e40a3dbc498be14b05ea4f
WJQSERVER <wjqserver@outlook.com>
2024-10-06 19:20:10 +0800

    update

--- config/config.go
+++ config/config.go
@@ -14,13 +14,17 @@ type Config struct {
 // LoadConfig 从 YAML 配置文件加载配置
 func LoadConfig(filePath string) (*Config, error) {
 	var config Config
-	data, err := os.ReadFile(filePath)
-	if err != nil {
-		return nil, err
-	}
-	err = yaml.Unmarshal(data, &config)
-	if err != nil {
+	if err := loadYAML(filePath, &config); err != nil {
 		return nil, err
 	}
 	return &config, nil
+}
+
+// LoadyamlConfig 从 YAML 配置文件加载配置
+func loadYAML(filePath string, out interface{}) error {
+	data, err := os.ReadFile(filePath)
+	if err != nil {
+		return err
+	}
+	return yaml.Unmarshal(data, out)
 }
