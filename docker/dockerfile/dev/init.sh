ce668d916f735d5b309a8f5d570e1f42e80987eb
里見 灯花 <172008506+satomitoka@users.noreply.github.com>
2024-12-04 20:24:49 +0800

    Update init.sh

--- docker/dockerfile/dev/init.sh
+++ docker/dockerfile/dev/init.sh
@@ -8,15 +8,15 @@ if [ ! -f /data/caddy/config/Caddyfile ]; then
 fi
 
 # 检查并复制 config.yaml
-if [ ! -f /data/${APPLICATION}/config/config.yaml ]; then
-    cp /data/${APPLICATION}/config.yaml /data/${APPLICATION}/config/config.yaml
+if [ ! -f /data/${APPLICATION}/config/config.toml ]; then
+    cp /data/${APPLICATION}/config.toml /data/${APPLICATION}/config/config.toml
 fi
 
 # 启动 Caddy
 /data/caddy/caddy run --config /data/caddy/config/Caddyfile > /data/${APPLICATION}/log/caddy.log 2>&1 &
 
 # 启动 Go 应用
-/data/${APPLICATION}/${APPLICATION} > /data/#{APPLICATION}/log/run.log 2>&1 &
+/data/${APPLICATION}/${APPLICATION} -cfg /data/${APPLICATION}/config/config.toml > /data/${APPLICATION}/log/run.log 2>&1 &
 
 # 保持脚本运行
 while true; do
